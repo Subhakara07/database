53b8569b0bb6858033ba68a184624b3b77f15563
Subhakara07 <subhakar.a07@gmail.com>
2026-06-17 15:59:34 -0500

    update the SQL files

--- scripts/V1__deploy.sql
+++ scripts/V1__deploy.sql
@@ -1,2 +1,11 @@
-@V1__check.sql
-@V1__create_table.sql
+
+-- Force SQL*Plus to exit immediately with a failure code back to Jenkins if a login/SQL error occurs
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+WHENEVER OSERROR EXIT FAILURE;
+
+-- Execute your downstream migration scripts
+@scripts/V1__check.sql
+@scripts/V1__create_table.sql
+
+-- Cleanly disconnect upon completion
+EXIT;
